f237662c6e7dca6d3b36bbf23eb2aaf5682fa5be
ydming <xm95zz@gmail.com>
2024-12-25 17:31:23 +0800

    update vue

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+    import { ElButton } from 'element-plus'
+  
+    
+</script>
+
+<template>
+	<main>
+		<div class="">home</div>
+        <ElButton>Button</ElButton>
+        
+	</main>
+</template>
